bb3a074712a6f7de491df01fcc25a1cdba4c55de
Jesus Avila <avijesus13@gmail.com>
2024-07-10 15:49:28 -0400

    refactored game with ui

--- script.js
+++ script.js
@@ -10,61 +10,89 @@ const GAME_WINNERS = {
   [GAME_CHOICES.scissors]: GAME_CHOICES.paper,
 };
 
+const ROUND_RESULTS = {
+  PLAYER: "PLAYER",
+  COMPUTER: "COMPUTER",
+  TIE: "TIE",
+};
+
+const playButton = document.querySelector("button");
+const choicesBoard = document.querySelector("#choices");
+const score = {
+  player: 0,
+  computer: 0,
+};
+
 const getComputerChoice = () => {
   const choices = Object.values(GAME_CHOICES);
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 };
 
-const getHumanChoice = () => {
-  const choice = prompt("Choose rock, paper, or scissors:");
+const playRound = (player1, player2) => {
+  if (player1 === player2) return ROUND_RESULTS.TIE;
 
-  if (!choice || !Object.values(GAME_CHOICES).includes(choice.toLowerCase())) {
-    alert("Please enter a valid choice");
-    return getHumanChoice();
+  return GAME_WINNERS[player1] === player2
+    ? ROUND_RESULTS.PLAYER
+    : ROUND_RESULTS.COMPUTER;
+};
+
+const updateScore = (result) => {
+  if (result === ROUND_RESULTS.PLAYER) {
+    score.player++;
+  } else if (result === ROUND_RESULTS.COMPUTER) {
+    score.computer++;
   }
+};
 
-  return choice.toLowerCase();
+const checkWinner = () => {
+  if (score.player === 5) {
+    return ROUND_RESULTS.PLAYER;
+  } else if (score.computer === 5) {
+    return ROUND_RESULTS.COMPUTER;
+  } else {
+    return null;
+  }
+};
+
+const finishGame = (winner) => {
+  score.player = 0;
+  score.computer = 0;
+  choicesBoard.textContent = "";
+  document.querySelector("#results").textContent = `WINNER: ${winner}`;
+  document.querySelector("#player-score").textContent = score["player"];
+  document.querySelector("#computer-score").textContent = score["computer"];
+
+  document.body.appendChild(playButton);
+};
+
+const displayResults = (choice, computerChoice, result) => {
+  const results = document.querySelector("#results");
+  results.textContent = `PLAYER CHOICE: ${choice.toUpperCase()} vs COMPUTER CHOICE: ${computerChoice.toUpperCase()}, WINNER: ${result}`;
+
+  document.querySelector("#player-score").textContent = score["player"];
+  document.querySelector("#computer-score").textContent = score["computer"];
 };
 
 const playGame = () => {
-  let humanScore = 0;
-  let computerScore = 0;
-  let turns = 1;
+  Object.values(GAME_CHOICES).forEach((choice) => {
+    const choiceButton = document.createElement("button");
+    choiceButton.textContent = choice.toUpperCase();
+    choiceButton.addEventListener("click", () => {
+      const computerChoice = getComputerChoice();
+      const result = playRound(choice, computerChoice);
+      updateScore(result);
+      displayResults(choice, computerChoice, result);
+      const winner = checkWinner();
 
-  const playRound = (player1, player2) => {
-    if (player1 === player2) {
-      console.log("It's a tie!");
-      return;
-    }
+      if (winner) {
+        finishGame(winner);
+      }
+    });
+    choicesBoard.appendChild(choiceButton);
+  });
 
-    if (GAME_WINNERS[player1] === player2) {
-      console.log("You win!");
-      return humanScore++;
-    } else {
-      console.log("You lose");
-      return computerScore++;
-    }
-  };
-
-  while (turns <= 5) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    console.log(`TURN ${turns}: ${humanChoice} vs ${computerChoice}`);
-    playRound(humanChoice, computerChoice);
-    turns++;
-  }
-
-  if (humanScore === computerScore) {
-    console.log("It's a tie!");
-  } else if (humanScore > computerScore) {
-    console.log("You win the game!");
-  } else {
-    console.log(`You lose the game!`);
-  }
-
-  console.log(`YOUR SCORE: ${humanScore} vs COMPUTER SCORE: ${computerScore}`);
+  document.body.removeChild(playButton);
 };
 
-const button = document.querySelector("button");
-button.addEventListener("click", playGame);
+playButton.addEventListener("click", playGame);
